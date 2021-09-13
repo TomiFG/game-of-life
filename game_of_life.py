0362eee0ae2aed7d583afbae6405c1d2ac460128
Tomas Gregoret <tomasfgregoret@gmail.com>
2021-09-13 10:25:16 -0300

    Added count_neighbors func

--- game_of_life.py
+++ game_of_life.py
@@ -34,3 +34,48 @@ def render(state):
         print(''.join(line), end='|\n') 
 
     print(divider)
+    
+def count_neighbors(state, x, y):
+    # counts the number of 'live cells' around a given one
+    count = 0    
+
+    if x==0:
+        if y==0:
+            count = state[y][x+1] + state[y+1][x] + state[y+1][x+1]
+            return count
+        if y==len(state)-1:
+            count = state[y-1][x] + state[y-1][x+1] + state[y][x+1]
+            return count
+
+        count += state[y-1][x] + state[y-1][x+1]
+        count += state[y][x+1]
+        count += state[y+1][x] + state[y+1][x+1]
+        return count
+
+    if x==len(state[0])-1:
+        if y==0:
+            count = state[y][x-1] + state[y+1][x-1] + state[y+1][x]
+            return count
+        if y==len(state)-1:
+            count = state[y-1][x-1] + state[y-1][x] + state[y][x-1]
+            return count
+
+        count += state[y-1][x-1] + state[y-1][x]
+        count += state[y][x-1]
+        count += state[y+1][x-1] + state[y+1][x]
+        return count
+
+    if y==0:
+        count += state[y][x-1] + state[y][x+1]
+        count += state[y+1][x-1] + state[y+1][x] + state[y+1][x+1]
+        return count
+    
+    if y==len(state)-1:
+        count += state[y-1][x-1] + state[y-1][x] + state[y-1][x+1]
+        count += state[y][x-1] + state[y][x+1]
+        return count
+    
+    count += state[y-1][x-1] + state[y-1][x] + state[y-1][x+1]
+    count += state[y][x-1] + state[y][x+1]
+    count += state[y+1][x-1] + state[y+1][x] + state[y+1][x+1]
+    return count
