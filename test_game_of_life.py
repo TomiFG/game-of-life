8a094ed9d2e7d5dd69b4f8a75c1af75a843108b0
Tomas Gregoret <tomasfgregoret@gmail.com>
2021-09-13 11:38:24 -0300

    Added test for next_state

--- test_game_of_life.py
+++ test_game_of_life.py
@@ -1,5 +1,5 @@
 import unittest
-from game_of_life import count_neighbors
+from game_of_life import count_neighbors, next_state
 
 
 state0 = [[0,0,0],
@@ -46,6 +46,63 @@ class TestGameOfLife(unittest.TestCase):
         self.assertEqual(count_neighbors(state3, 0, 1), 2)
         self.assertEqual(count_neighbors(state3, 2, 1), 2)
         self.assertEqual(count_neighbors(state3, 1, 2), 2)
+
+    def test_next_state(self):
+
+        expected_output_s0 = [
+                [0,0,0],
+                [0,0,0],
+                [0,0,0]
+                ]
+
+        actual_output_s0 = next_state(state0)
+        self.assertEqual(expected_output_s0, actual_output_s0)
+
+        expected_output_s1 = [
+                [1,0,1],
+                [0,0,0],
+                [1,0,1]
+                ]
+
+        actual_output_s1 = next_state(state1)
+        self.assertEqual(expected_output_s1, actual_output_s1)
+
+        expected_output_s2 = [
+                [0,0,0],
+                [0,0,0],
+                [0,0,0]
+                ]
+
+        actual_output_s2 = next_state(state2)
+        self.assertEqual(expected_output_s2, actual_output_s2)
+
+        expected_output_s3 = [
+                [0,0,0],
+                [0,0,0], 
+                [0,0,0]
+                ]
+
+        actual_output_s3 = next_state(state3)
+        self.assertEqual(expected_output_s3, actual_output_s3)
+
+        expected_output_s4 = [
+                [0,1,0],
+                [1,0,1],
+                [0,1,0]
+                ]
+
+        actual_output_s4 = next_state(state4)
+        self.assertEqual(expected_output_s4, actual_output_s4)
+
+        expected_output_s5 = [
+                [1,1,1],
+                [1,0,1],
+                [1,1,1]
+                ]
+
+        actual_output_s5 = next_state(state5)
+        self.assertEqual(expected_output_s5, actual_output_s5)
+        
 
 
 if __name__ == '__main__':
